a707de2c47405af262497cbfe7d2d2d1620770ae
Sydney Andre <sydneyandre@Sydneys-MBP.attlocal.net>
2024-04-23 18:16:51 -0500

    updated navbar with dropdown links and icons

--- my-portfolio/src/Components/Navbar.tsx
+++ my-portfolio/src/Components/Navbar.tsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import Nav from 'react-bootstrap/Nav';
 import NavDropdown from 'react-bootstrap/NavDropdown';
-
-
+import github from '../icons/github.svg'
+import linkedin from '../icons/linkedin.svg'
+import email from '../icons/email.svg'
 
 function NavigationBar (){
     return(
-        <div>
-            <Nav variant="underline">
+        <div className='standard-marg-left'>
+            <Nav variant="underline" defaultActiveKey="/">
                 <Nav.Item>
-                    <Nav.Link href="/home">About</Nav.Link>
+                    <Nav.Link href="/home" >About</Nav.Link>
                 </Nav.Item>
                 <Nav.Item>
                     <Nav.Link href="/home">Projects</Nav.Link>
@@ -18,9 +19,30 @@ function NavigationBar (){
                     <Nav.Link href="/home">Resume</Nav.Link>
                 </Nav.Item>
                 <NavDropdown title="Let's Chat" id="nav-dropdown">
-                    <NavDropdown.Item  href="mailto:sydneyandre.dev@gmail.com" target="_blank" rel="noreferrer">Email With Me</NavDropdown.Item>
-                    <NavDropdown.Item  href="http://linkedin.com/in/sydney-andre" target="_blank" rel="noreferrer">Link With Me</NavDropdown.Item>
-                    <NavDropdown.Item href="http://github.com/saandre0217" target="_blank" rel="noreferrer">Git with Me</NavDropdown.Item>
+                    <NavDropdown.Item  href="mailto:sydneyandre.dev@gmail.com" target="_blank" rel="noreferrer">
+                        <div className="custom-row">
+                            <img style={{ marginRight:'.25rem'}} src={email}/>
+                            <div>
+                                Email With Me
+                            </div>
+                        </div>      
+                    </NavDropdown.Item>
+                    <NavDropdown.Item href="http://linkedin.com/in/sydney-andre" target="_blank" rel="noreferrer">
+                    <div className="custom-row">
+                    <img style={{ marginRight:'.25rem'}} src={linkedin}/>
+                    <div>
+                        Link With Me
+                    </div>
+                    </div>
+                    </NavDropdown.Item>
+                    <NavDropdown.Item  href="http://github.com/saandre0217" target="_blank" rel="noreferrer">
+                    <div className="custom-row">
+                    <img style={{ marginRight:'.25rem'}} src={github}/>
+                    <div>
+                        Git With Me
+                    </div>
+                    </div>
+                    </NavDropdown.Item>
                 </NavDropdown>
             </Nav>            
         </div>
