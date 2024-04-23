a1b87aacfe3b9dc98b284c1cdac038b6cdd2e18a
Sydney Andre <sydneyandre@Sydneys-MBP.attlocal.net>
2024-04-23 12:21:19 -0500

    installed reactbootstrap in the correct directory; navbar framework set up; uploaded and changed favicon

--- my-portfolio/src/App.tsx
+++ my-portfolio/src/App.tsx
@@ -2,14 +2,13 @@ import React from 'react';
 import './App.css';
 import NavigationBar from './Components/Navbar';
 
-
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-       <div className="title">Sydney Andre</div>
+        <div className="title">Sydney Andre</div>
       </header>
-       <NavigationBar/> 
+            <NavigationBar />
     </div>
   );
 }
--- my-portfolio/src/Components/Navbar.tsx
+++ my-portfolio/src/Components/Navbar.tsx
@@ -1,11 +1,28 @@
-import { Button } from 'react-bootstrap';
+import React from 'react';
+import Nav from 'react-bootstrap/Nav';
+import NavDropdown from 'react-bootstrap/NavDropdown';
 
 
 
 function NavigationBar (){
     return(
         <div>
-            <Button>Button</Button>
+            <Nav variant="underline">
+                <Nav.Item>
+                    <Nav.Link href="/home">About</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                    <Nav.Link href="/home">Projects</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                    <Nav.Link href="/home">Resume</Nav.Link>
+                </Nav.Item>
+                <NavDropdown title="Let's Chat" id="nav-dropdown">
+                    <NavDropdown.Item  href="mailto:sydneyandre.dev@gmail.com" target="_blank" rel="noreferrer">Email With Me</NavDropdown.Item>
+                    <NavDropdown.Item  href="http://linkedin.com/in/sydney-andre" target="_blank" rel="noreferrer">Link With Me</NavDropdown.Item>
+                    <NavDropdown.Item href="http://github.com/saandre0217" target="_blank" rel="noreferrer">Git with Me</NavDropdown.Item>
+                </NavDropdown>
+            </Nav>            
         </div>
     )
 }
